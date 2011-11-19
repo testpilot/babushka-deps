424a2ea3b4f0bf67c8dd1c9a91f51a4901afc93a
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-11-19 13:55:09 +1100

    Added core software

---
+++ worker/core-software.rb
@@ -0,0 +1,37 @@
+packages = [
+  'lsof',
+  'iptables',
+  'jwhois',
+  'whois',
+  'curl',
+  'wget',
+  'rsync',
+  'jnettop',
+  'nmap',
+  'traceroute',
+  'ethtool',
+  'tcpdump',
+  'elinks',
+  'lynx'
+].each do |package|
+  dep [package, 'managed'].join('.')
+end
+
+packages_without_binary = [
+  'iproute',
+  'iputils-ping',
+  'netcat-openbsd',
+  'bind9-host',
+  'libreadline5-dev',
+  'libssl-dev',
+  'libxml2-dev',
+  'libxslt1-dev',
+  'zlib1g-dev'
+].each { |p|
+  dep [p, 'managed'].join('.') { provides [] }
+}
+
+dep('core dependencies') {
+  requires (packages + packages_without_binary).map { |p| "#{p}.managed" }
+}
+
