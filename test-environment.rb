23a36040010acfb256cd4c1a5eb3b704b9e0584c
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-11-19 14:09:30 +1100

    Corrected language pack dep

--- test-environment.rb
+++ test-environment.rb
@@ -1,6 +1,6 @@
 dep('test environment') {
   requires [
-    'language-pack-en configured',
+    'language-pack-en.managed',
     'core dependencies',
     'build essential installed',
     'scons.managed',
