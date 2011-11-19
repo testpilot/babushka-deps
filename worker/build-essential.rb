8f009e5ca27de6eb14a4048c3ecd8342502ec162
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-11-19 13:55:02 +1100

    Added build essential

---
+++ worker/build-essential.rb
@@ -0,0 +1,9 @@
+dep('build essential installed') {
+  requires [
+    'build-essential.managed',
+    'binutils-doc.managed',
+    'autoconf.managed',
+    'flex.managed',
+    'bison.managed'
+  ]
+}
