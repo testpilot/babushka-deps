8f40df782e61c1e52f525b15d3978d8ab8b859ae
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-11-19 14:21:58 +1100

    Corrected paths

--- worker/timetrap.rb
+++ worker/timetrap.rb
@@ -1,7 +1,7 @@
 dep('timetrap configured') {
   met? {
-    "/usr/local/bin/timetrap".exists? &&
-    "/etc/profile.d/timetrap.sh".exists?
+    "/usr/local/bin/timetrap".p.exists? &&
+    "/etc/profile.d/timetrap.sh".p.exists?
   }
   meet {
     render_erb("timetrap/timetrap.erb", :to => "/usr/local/bin/timetrap", :sudo => true)
