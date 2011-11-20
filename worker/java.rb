10ee2ab7a9d5d4eff70b02e23c0c035159e30f38
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-11-20 16:48:06 +1100

    Fixed java sourcing

--- worker/java.rb
+++ worker/java.rb
@@ -7,6 +7,10 @@ dep('java installed') {
 }
 
 dep 'java.managed' do
+  before {
+    shell "add-apt-repository \"deb http://archive.canonical.com/ lucid partner\"", :sudo => true
+    Babushka::AptHelper.update_pkg_lists
+  }
   installs { via :apt, 'sun-java6-jre' }
   after { shell "set -Ux JAVA_HOME /usr/lib/jvm/java-6-sun" }
 end
