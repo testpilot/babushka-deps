f2a9a2d6cca65536625e3412822523530ecba66b
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-12-19 16:34:12 +1100

    Added database driver gems

--- ruby-environment.rb
+++ ruby-environment.rb
@@ -8,7 +8,9 @@ dep('ruby environment', :global_ruby_versions){
     'therubyracer.global_gem'.with(global_ruby_versions),
     'nokogiri.global_gem'.with(global_ruby_versions),
     'gherkin.global_gem'.with(global_ruby_versions),
-    'json.global_gem'.with(global_ruby_versions)
+    'json.global_gem'.with(global_ruby_versions),
+    'pg.global_gem'.with(global_ruby_versions),
+    'mysql2.global_gem'.with(global_ruby_versions)
   ]
 }
 
@@ -49,5 +51,15 @@ dep('json.global_gem', :ruby_versions) {
 dep('gherkin.global_gem', :ruby_versions) {
   rubies *('1.8.7, 1.9.2, 1.9.3'.to_s.split(',').map(&:chomp))
   versions '2.5.1'
+}
+
+dep('mysql2.global_gem', :ruby_versions) {
+  rubies *('1.8.7, 1.9.2, 1.9.3'.to_s.split(',').map(&:chomp))
+  versions '0.3.11', '0.3.10', '0.3.9', '0.3.8'
+}
+
+dep('pg.global_gem', :ruby_versions) {
+  rubies *('1.8.7, 1.9.2, 1.9.3'.to_s.split(',').map(&:chomp))
+  versions '0.12.0', 0.11.0, '0.10.1'
 }
 
