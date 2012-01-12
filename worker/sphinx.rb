75a0b6384cbb3105ef299f1106e264fb5a15dd4a
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-01-12 15:50:32 +1100

    Added libstemmer

--- worker/sphinx.rb
+++ worker/sphinx.rb
@@ -27,8 +27,12 @@ dep('sphinx installed', :version) {
     # shell "mkdir -p #{tmp_install_dir} && cd #{tmp_install_dir}"
     
     # Download and extract, entering new directory
-    Babushka::Resource.extract(sphinx_src_path)
-    Babushka::Resource.extract('http://snowball.tartarus.org/dist/libstemmer_c.tgz')
+    sphinx_dir      = Babushka::Resource.extract(sphinx_src_path).filename
+    
+    Babushka::Resource.get('http://snowball.tartarus.org/dist/libstemmer_c.tgz') do |download_path|
+      shell "cp #{download_path} #{sphinx_dir}"
+    end
+    
     shell "./configure --with-mysql --with-pgsql --with-libstemmer --prefix=#{path} "
     shell "make && make install"
     
