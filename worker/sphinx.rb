d261ee0023ac431f54870c207b4770d9c8364c59
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-01-12 16:09:01 +1100

    Added logging

--- worker/sphinx.rb
+++ worker/sphinx.rb
@@ -1,10 +1,13 @@
 dep('sphinx multiple versions installed') {
-  requires 'sphinx installed'.with('2.0.1-beta'), 'sphinx installed'.with('1.10-beta'), 'sphinx installed'.with('sphinx 0.9.9')
+  requires 'sphinx installed'.with('2.0.1-beta', 'true'), 'sphinx installed'.with('1.10-beta'), 'sphinx installed'.with('sphinx 0.9.9')
 }
 
 dep('libmysql++-dev.managed') { provides [] }
 
-dep('sphinx installed', :version) {
+dep('sphinx installed', :version, :main) {
+  main.default!('false')
+  version.default!('2.0.1-beta')
+  
   requires 'libmysql++-dev.managed'
   
   def sphinx_src_path
@@ -31,7 +34,14 @@ dep('sphinx installed', :version) {
       log "Sphinx directory is: #{shell('pwd')}"
     
       log_shell "Configuring Sphinx #{version} with stemmer support for installation into #{path}", "./configure --with-mysql --with-pgsql --with-libstemmer --prefix=#{path}"
-      log_shell "Compiling Sphinx", "make && make install", :sudo => true
+      log_shell "Compiling Sphinx", "make"
+      log_shell "Installing", "sudo make install"
+      
+      if main == 'true' # ugly hax
+        %w( indexer indextool search searchd spelldump ).each do |binary|
+          log_shell "Linking #{binary}", "sudo ln -s #{path / binary} /usr/local/bin/#{binary}"
+        end
+      end
     end
   }
 }
