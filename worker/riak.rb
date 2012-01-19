f9f3774f8e968bc956e43ddd6032980976377ef2
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-01-19 14:53:13 +1100

    Converted riak to 64 and 32 bit installer

--- worker/riak.rb
+++ worker/riak.rb
@@ -8,8 +8,15 @@ dep('riak installed from package'){
   }
 
   meet {
-    Babushka::Resource.get('http://downloads.basho.com.s3-website-us-east-1.amazonaws.com/riak/1.0/1.0.0/riak_1.0.0-1_i386.deb') do |download_path|
-      log_shell("Installing Riak", "dpkg -i #{download_path}", :log => true, :sudo => true)
+    if shell?("uname -m | grep x86_64")
+      # We're 64bit baby!
+      Babushka::Resource.get('http://downloads.basho.com/riak/riak-1.0.2/riak_1.0.2-1_amd64.deb') do |download_path|
+        log_shell("Installing Riak", "dpkg -i #{download_path}", :log => true, :sudo => true)
+      end
+    else
+      Babushka::Resource.get('http://downloads.basho.com/riak/riak-1.0.2/riak_1.0.2-1_i386.deb') do |download_path|
+        log_shell("Installing Riak", "dpkg -i #{download_path}", :log => true, :sudo => true)
+      end
     end
   }
 }
