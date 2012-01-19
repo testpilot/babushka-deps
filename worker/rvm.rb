876b1b0b6105cacd357959a6b47ac53e6db97ce1
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-01-19 16:55:58 +1100

    Install JSON if missing

--- worker/rvm.rb
+++ worker/rvm.rb
@@ -156,7 +156,7 @@ meta :gem_installed do
   end
 
   def versions
-    @versions ||= load_gem_version_data(gem_name).reject {|version| skipped_versions.map(&:to_i).include?(version) }[0, depth]
+    @versions ||= load_gem_version_data(gem_name).reject {|version| skipped_versions.include?(version) }[0, depth]
   end
 
   def load_gem_version_data(gem_name)
@@ -177,7 +177,7 @@ meta :gem_installed do
       select {|gem| gem['prerelease'] == false }.
       # Restrict to MRI
       select {|gem| gem['platform'] == 'ruby' }.
-      map {|gem| gem['number'] }.map(&:to_i).sort
+      map {|gem| gem['number'] }.sort
   end
 
   def installed_versions
