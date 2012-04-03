6ffebd6616f93ddb1b236313bb4e5bc8aaaf742e
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-04-03 15:54:30 +1000

    Updated Firefox dep to use new keys

--- worker/firefox.rb
+++ worker/firefox.rb
@@ -2,14 +2,10 @@ dep('firefox installed') {
   requires 'firefox.managed'
 }
 
-# sudo add-apt-repository ppa:mozillateam/firefox-stable
-# 
-# sudo apt-get update
-# sudo apt-get upgrade
-
 dep('firefox.managed') {
   before {
-    shell "sudo add-apt-repository ppa:mozillateam/firefox-stable"
+    # shell "sudo add-apt-repository ppa:mozillateam/firefox-stable"
+    shell "sudo add-apt-repository ppa:mozillateam/firefox-next"
     shell "sudo apt-get update"
   }
   installs { via :apt, 'firefox' }
