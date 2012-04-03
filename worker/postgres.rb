628457883245161c9b2f6c471ce049b38cca077c
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-04-03 15:59:27 +1000

    Fixed postgres.managed

--- worker/postgres.rb
+++ worker/postgres.rb
@@ -3,14 +3,15 @@ dep('postgresql installed'){
 }
 
 dep 'postgres.managed', :version do
-  version.default!('9.1')
-
-  requires {
-    on :apt, 'set.locale', 'postgres.ppa'
-    on :brew, 'set.locale'
+  version.default('9.1')
+  # Assume the installed version if there is one
+  version.default!(shell('psql --version').val_for('psql (PostgreSQL)')[/^\d\.\d/]) if which('psql')
+  requires 'set.locale'
+  requires_when_unmet {
+    on :apt, 'ppa'.with('ppa:pitti/postgresql')
   }
   installs {
-    via :apt, ["postgresql-#{owner.version}", "libpq-dev", 'postgresql-client']
+    via :apt, ["postgresql-#{owner.version}", "libpq-dev"]
     via :brew, "postgresql"
   }
   provides "psql ~> #{version}.0"
