78dbf5961db17389cc6169fa192adacf39e7deef
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-04-03 14:57:29 +1000

    Added before step to git.managed to ensure we have babushka.list

--- worker/git.rb
+++ worker/git.rb
@@ -1,4 +1,8 @@
 dep 'git.managed' do
+  before {
+    shell "touch /etc/apt/sources.list.d/babushka.list", :sudo => true
+    shell "chown ubuntu:ubuntu /etc/apt/sources.list.d/babushka.list", :sudo => true
+  }
   requires 'ppa'.with('ppa:git-core/ppa')
   installs 'git'
   provides 'git >= 1.7.4.1'
