9b9dbfa8e8420d1592bcae9b0a2351649aea175e
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-04-09 20:37:09 +1000

    Experimenting with before and after hooks for config permissions

--- worker/git.rb
+++ worker/git.rb
@@ -9,12 +9,17 @@ dep('git installed') {
 
 dep 'git.managed' do
   before {
-    shell "touch /etc/apt/sources.list.d/babushka.list", :sudo => true
+    shell "sudo touch /etc/apt/sources.list.d/babushka.list"
     shell "sudo chown ubuntu:ubuntu  /etc/apt/sources.list.d/babushka.list"
   }
+
   requires 'ppa'.with('ppa:git-core/ppa')
   installs 'git'
   provides 'git >= 1.7.4.1'
+
+  after {
+    shell "sudo chown root:root /etc/apt/sources.list.d/babushka.list"
+  }
 end
 
 
