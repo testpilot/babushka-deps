388fc1c5ea3f5640e105ab706c77d8fc4041a8e2
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-04-09 20:41:15 +1000

    Rollback to using apt to install git core

--- worker/git.rb
+++ worker/git.rb
@@ -1,25 +1,25 @@
 dep('git installed') {
-  requires 'git.managed'
+  requires 'git-core.managed'
 }
 
-# dep('git-core.managed'){
-#   installs []
-#   provides 'git >= 1.7.0.4'
-# }
+dep('git-core.managed'){
+  installs ['git-core', 'git-man']
+  provides 'git >= 1.7.0.4'
+}
 
-dep 'git.managed' do
-  before {
-    shell "sudo touch /etc/apt/sources.list.d/babushka.list"
-    shell "sudo chown ubuntu:ubuntu  /etc/apt/sources.list.d/babushka.list"
-  }
-
-  requires 'ppa'.with('ppa:git-core/ppa')
-  installs 'git'
-  provides 'git >= 1.7.4.1'
-
-  after {
-    shell "sudo chown root:root /etc/apt/sources.list.d/babushka.list"
-  }
-end
-
-
+# dep 'git.managed' do
+#   before {
+#     shell "sudo touch /etc/apt/sources.list.d/babushka.list"
+#     shell "sudo chown ubuntu:ubuntu  /etc/apt/sources.list.d/babushka.list"
+#   }
+# 
+#   requires 'ppa'.with('ppa:git-core/ppa')
+#   installs 'git'
+#   provides 'git >= 1.7.4.1'
+# 
+#   after {
+#     shell "sudo chown root:root /etc/apt/sources.list.d/babushka.list"
+#   }
+# end
+# 
+# 
