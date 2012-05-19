9fed0448d145a9f3c6418f4ec05e2c7cec07c476
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-05-19 14:21:17 +1000

    Added Phantom.js

---
+++ worker/phantomjs.rb
@@ -0,0 +1,31 @@
+dep("phantom.js installed") {
+  requires 'freetype.managed',
+            'fontconfig.managed',
+            'chrpath.managed',
+            'libssl-dev.managed',
+            'libfontconfig1-dev.managed'
+
+  met? { in_path?("phantomjs") }
+
+  before {
+    shell "rm -rf /tmp/phantomjs"
+  }
+
+  meet {
+    # sudo apt-get install build-essential chrpath git-core libssl-dev libfontconfig1-dev
+    # git clone git://github.com/ariya/phantomjs.git && cd phantomjs
+    # git checkout 1.5
+    # ./build.sh
+
+    log_shell "CLoning Phantom.js", "git clone git://github.com/ariya/phantomjs.git /tmp/phantomjs/ && cd /tmp/phantomjs"
+    shell "git checkout 1.5" and
+    shell "./build.sh"
+  }
+}
+
+dep('freetype.managed')
+dep('fontconfig.managed')
+dep('chrpath.managed')
+dep('chrpath.managed')
+dep('libssl-dev.managed')
+dep('libfontconfig1-dev.managed')
